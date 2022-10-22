bae87ffbbbecb5f3fe6891ac9206e4adfa358121
flou-ainan <flouainan@gmail.com>
2022-10-21 23:02:56 -0300

    readme update

--- with-tailwindcss-app/.next/static/edZ6fv3B3XBsKqtclPYsI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-3556705ba9bb28af.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
